4882474ae4a1c3893777f6b989d7cdc019177d79
송재경 <jake@xlgames.com>
2019-02-25 19:41:14 +0900

    first commit

---
+++ Sample.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default class Sample extends Vue {
+  a: string;
+
+  sample_method() {
+    return this.a;
+  }
+}
+</script>
